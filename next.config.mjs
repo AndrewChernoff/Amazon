f082d41b4b4cc67ef9c3ab5394c2598d51ce995a
Andrew Chernoff <whitedrew538@gmail.com>
2024-07-03 16:17:19 +0500

    changed next.config

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,20 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["www.hatchwise.com", "media.graphassets.com"],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'www.hatchwise.com',
+        port: '',
+        pathname: '/wp-content/uploads/2022/05/amazon-logo-1024x683.png',
+      },
+      {
+        protocol: 'https',
+        hostname: 'media.graphassets.com',
+        port: '',
+        pathname: 'images/I/61xnEcip5RL._SX3000_.jpg',
+      },
+    ],
   },
   compiler: {
     styledComponents: true,
